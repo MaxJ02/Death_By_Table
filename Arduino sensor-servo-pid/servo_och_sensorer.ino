a0813c3ecbd9e7eb58f615faef344edc93b01f95
DenizKula <121364706+DenizKula@users.noreply.github.com>
2023-04-23 00:35:33 +0200

    Reverse och kör signal
    Lade till en reverse funktion som vänder på styrningen när framsensorn känner av ett avstånd på mindre än 20cm i 3 sekunder. Avstånd och tiden borde finjusteras efter test på banan. När reverse variabeln blir true skickas det även en signal ut på pin8 som motor arduino kan använda för att vända motorriktning. Satte även pin 10 som inport för att känna av startsignal från motor arduinon, så att servo inte sitter och rycker innan racet börjar.

--- Arduino sensor-servo-pid/servo_och_sensorer.ino
+++ Arduino sensor-servo-pid/servo_och_sensorer.ino
@@ -1,5 +1,10 @@
 #include "header.h"
 
+#define RUN_SIGNAL 10
+#define REVERSE 8
+#define REVERSE_COUNT 30 //3 sekunder
+#define RESTORE_COUNT 20 //2 sekunder
+
 // Servo variables
 Servo myServo;
 double angle;
@@ -10,8 +15,10 @@ const uint8_t sensorCount = 3;
 const uint8_t xshutPins[sensorCount] = { 5, 6, 7 };
 VL53L1X sensors[sensorCount];
 int distance[sensorCount];
-int totalDistance = 0;
-int averageDistance = 0;
+bool reverse = false;
+uint16_t timer_counter = 0;
+
+
 
 /********************************************************************************
 * servo_run: Reads input values for left and right TOF sensor, regulates the
@@ -22,20 +29,80 @@ int averageDistance = 0;
 ********************************************************************************/
 void servo_run(struct servo* self)
 {
-   self->tof_left.val = sensors[1].read();
-   self->tof_right.val = sensors[2].read();
-
    pid_regulate(&self->pid, servo_input_mapped(self));
    angle = self->pid.output;
-   //angle = map(angle,0,1023,0,180);
+   //angle = map(angle,0,180,0,1023);
    myServo.write(angle);
    return;
 }
 
+/********************************************************************************
+* check_reverse: Om fram signalen har varit lägre än 200mm i 3 sekunder sätts 
+*                reverse variabeln till true.            
+********************************************************************************/
+void check_reverse()
+{
+  if(reverse == true)
+  {
+    timer_counter++;
+    if(timer_counter >= RESTORE_COUNT)
+    {
+      timer_counter = 0;
+      reverse = false;
+      digitalWrite(REVERSE, 0);
+    }
+  }
+  else if(sensors[2].read() <200 )
+  {
+    timer_counter++;
+    if(timer_counter >= REVERSE_COUNT)
+    {
+      timer_counter = 0;
+      reverse = true;
+      digitalWrite(REVERSE, 1);
+    }
+  }
+  else timer_counter = 0;
+}
+
+/********************************************************************************
+* print_values: Skriver ut värden på sensorer och vilken vinkeln som servon
+*               är inställd mot. Skriver även ut om bil är i reverse.
+********************************************************************************/
+void print_values()
+{
+  Serial.print("Left: ");
+  Serial.print(servo1.tof_left.val);
+  Serial.print('\t');
+  Serial.print("Right: ");
+  Serial.print(servo1.tof_right.val);
+  Serial.print('\t');
+  Serial.print("Angle: ");
+  Serial.print(servo1.pid.output);
+  Serial.print('\t');
+  Serial.print("Front distance: ");
+  Serial.print(sensors[2].read());
+  Serial.print('\t');
+  if(reverse)
+  {
+    Serial.print("Reverse? Yes \t");
+  }
+  else Serial.print("Reverse? No \t");
+  Serial.print(timer_counter);
+  Serial.println();
+}
+
+
 void setup()
 {
-  // Servo setup
+  //Start signal
+  pinMode(RUN_SIGNAL, INPUT);
+
+ // Servo setup
   myServo.attach(9);
+
+  //Reverse signal
+  pinMode(REVERSE, OUTPUT);
 
   // Sensor setup
   while (!Serial) {}
@@ -43,19 +110,14 @@ void setup()
   Wire.begin();
   Wire.setClock(400000); // use 400 kHz I2C
 
-  // Disable/reset all sensors by driving their XSHUT pins low.
   for (uint8_t i = 0; i < sensorCount; i++)
   {
     pinMode(xshutPins[i], OUTPUT);
     digitalWrite(xshutPins[i], LOW);
   }
 
-  // Enable, initialize, and start each sensor, one by one.
   for (uint8_t i = 0; i < sensorCount; i++)
   {
-    // Stop driving this sensor's XSHUT low. This should allow the carrier
-    // board to pull it high. (We do NOT want to drive XSHUT high since it is
-    // not level shifted.) Then wait a bit for the sensor to start up.
     pinMode(xshutPins[i], INPUT);
     delay(10);
 
@@ -67,38 +129,35 @@ void setup()
       while (1);
     }
 
-    // Each sensor must have its address changed to a unique value other than
-    // the default of 0x29 (except for the last one, which could be left at
-    // the default). To make it simple, we'll just count up from 0x2A.
     sensors[i].setAddress(0x2A + i);
 
     sensors[i].startContinuous(50);
   }
-  //PID initiering på servo
-  servo_init(&servo1,90,45,135,0,500,1,001,01);
 
+  //PID initiering på servo
+  servo_init(&servo1,90,35,135,0,500,1,0,0.1);
 }
+
 
 
 void loop()
 {
-
-  
-  //Run servo and pid regulation
   while(1)
   {
+  check_reverse();
+  if(reverse)
+  {
+    servo1.tof_left.val = sensors[1].read();
+    servo1.tof_right.val = sensors[0].read();
+    servo_run(&servo1);
+    print_values();
+  }
+  else
+  {
+  servo1.tof_left.val = sensors[0].read();
+  servo1.tof_right.val = sensors[1].read();
   servo_run(&servo1);
-  Serial.print("Left: ");
-  Serial.print(servo1.tof_left.val);
-  Serial.print('\t');
-  Serial.print("Right: ");
-  Serial.print(servo1.tof_right.val);
-  Serial.print('\t');
-  Serial.print("Angle: ");
-  Serial.print(servo1.pid.output);
-  Serial.print('\t');
-  Serial.print("Mapped input: ");
-  Serial.print(servo_input_mapped(&servo1));
-  Serial.println();
+  print_values();
+  }
   }
 }
