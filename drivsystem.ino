49120b8f33c827b2590bc9d0bdca42ad3d963567
Max <114068285+MaxJ02@users.noreply.github.com>
2023-05-05 11:49:03 +0200

    Update drivsystem.ino
    några nya funktioner.

--- drivsystem.ino
+++ drivsystem.ino
@@ -2,6 +2,7 @@
 // motor one
 #define KillSwitch 2 // Pin nummer för killswitch på startmodul
 #define RemoteStart 3 // Pin nummer för start modulens remote start
+#define StartSignal 6 // Pin nummer som signalerar till styr arduino att starta
 
 #define MOTOR_FORWARD  10
 #define MOTOR_BACKWARD 11 
@@ -21,6 +22,7 @@ static inline void motor_backward(const uint8_t pwm_value)
 static inline void motor_wait_for_start(void)
 {
   while ((digitalRead(RemoteStart) && digitalRead(KillSwitch)) == 0);
+  digitalWrite(StartSignal, 1);
   return;
 }
 
@@ -29,12 +31,23 @@ void setup()
   // Initierar pinnar för motorstyrning samt startmodul.
   pinMode(RemoteStart, INPUT);
   pinMode(KillSwitch, INPUT);
+  pinMode(StartSignal, OUTPUT);
   motor_wait_for_start();
 }
 
+//Sätter motorhastighet  i procent mellan 0-100%
+void set_speed(uint8_t speed)
+{
+  speed = map(speed, 0,100,0,255);
+  analogWrite(MOTOR_FORWARD, speed);
+  digitalWrite(MOTOR_BACKWARD, 0);
+}
+
+
+
 void demo()
 {
-  for (int i = 0; i < 255; i++)
+  for (int i = 0; i < 200; i++)
   {
     motor_forward(i);
     delay(20);
@@ -42,7 +55,8 @@ void demo()
 }
 void loop()
 {
-  demo();
+  //demo();
+  set_speed(80); // 70 är default
   while((digitalRead(RemoteStart) && digitalRead(KillSwitch)) == 1)
   {
 
@@ -51,5 +65,6 @@ void loop()
   {
     digitalWrite(MOTOR_BACKWARD, 0);
     digitalWrite(MOTOR_FORWARD, 0);
+    digitalWrite(StartSignal, 0);
   }
 }
