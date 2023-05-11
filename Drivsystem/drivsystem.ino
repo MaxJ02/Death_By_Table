31458ffc56bd8671fbee88875ee20cd2a84e9caf
MaxJ02 <114068285+MaxJ02@users.noreply.github.com>
2023-05-11 09:27:17 +0200

    Add files via upload

---
+++ Drivsystem/drivsystem.ino
@@ -0,0 +1,71 @@
+// connect motor controller pins to Arduino digital pins
+// motor one
+#define KillSwitch 2 // Pin nummer för killswitch på startmodul
+#define RemoteStart 3 // Pin nummer för start modulens remote start
+#define StartSignal A0 // Pin nummer som signalerar till styr arduino att starta
+#define REVERSE A1     // pin nummer för back-funktion.
+#define BOOST A2       // pin nummer för boost funktion
+#define MOTOR_FORWARD  10 // pin nummer för motor_forward pin
+#define MOTOR_BACKWARD 11  // pin nummer för motor_backward pin
+
+static inline void motor_forward(const uint8_t pwm_value)
+{
+  analogWrite(MOTOR_FORWARD, pwm_value);
+  digitalWrite(MOTOR_BACKWARD, 0);
+}
+
+static inline void motor_backward(const uint8_t pwm_value)
+{
+  digitalWrite(MOTOR_FORWARD, 0);
+  analogWrite(MOTOR_BACKWARD, pwm_value);
+}
+
+static inline void motor_wait_for_start(void)
+{
+  while ((digitalRead(RemoteStart) && digitalRead(KillSwitch)) == 0);
+  digitalWrite(StartSignal, 1);
+  return;
+}
+
+void setup()
+{
+  // Initierar pinnar för motorstyrning samt startmodul.
+  pinMode(RemoteStart, INPUT);
+  pinMode(KillSwitch, INPUT);
+  pinMode(StartSignal, OUTPUT);
+  motor_wait_for_start();
+}
+
+//Sätter motorhastighet  i procent mellan 0-100%
+void set_speed(uint8_t speed)
+{
+  speed = map(speed, 0,100,0,255);
+  analogWrite(MOTOR_FORWARD, speed);
+  digitalWrite(MOTOR_BACKWARD, 0);
+}
+
+
+
+void demo()
+{
+  for (int i = 0; i < 200; i++)
+  {
+    motor_forward(i);
+    delay(20);
+  } 
+}
+void loop()
+{
+  //demo();
+  set_speed(60); // 70 var default med svagare LIPO, 60 Nuvarande default med 11v LIPO.
+  while((digitalRead(RemoteStart) && digitalRead(KillSwitch)) == 1)
+  {
+
+  }
+  while((digitalRead(RemoteStart) && digitalRead(KillSwitch)) == 0)
+  {
+    digitalWrite(MOTOR_BACKWARD, 0);
+    digitalWrite(MOTOR_FORWARD, 0);
+    digitalWrite(StartSignal, 0);
+  }
+}
---
+++ Styrsystem/header.hpp
@@ -0,0 +1,13 @@
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#include <Servo.h>
+#include <Wire.h>
+#include <VL53L1X.h>
+#include "tof.hpp"
+#include "pid.hpp"
+#include "servo.hpp"
+
+
+
+#endif /* HEADER_H_ */
