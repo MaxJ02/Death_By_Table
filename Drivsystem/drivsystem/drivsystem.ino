dd3aa27e437840033c1f9157f9a302af1186be4c
Linus Oskarsson <112986911+F3nder@users.noreply.github.com>
2023-06-07 14:07:36 +0200

    Update drivsystem.ino

--- Drivsystem/drivsystem/drivsystem.ino
+++ Drivsystem/drivsystem/drivsystem.ino
@@ -1,10 +1,13 @@
-// connect motor controller pins to Arduino digital pins
-// motor one
+/********************************************************************************
+* Drivsystem: Kod för att styra körriktning och motorhastighet via: 
+*             Motordrivare L298 dubbel H-Brygga 5-35V 2A.
+*             Samt Remotstart och stop.
+********************************************************************************/
 #define KillSwitch 2 // Pin nummer för killswitch på startmodul
 #define RemoteStart 3 // Pin nummer för start modulens remote start
 #define StartSignal A0 // Pin nummer som signalerar till styr arduino att starta
-#define BOOST A1     // pin nummer för back-funktion.
-#define TURN A2       // pin nummer för boost funktion
+#define BOOST A1     // pin nummer för boost funktion.
+#define TURN A2       // pin nummer för att sakta ner vid sväng.
 #define MOTOR_FORWARD  10 // pin nummer för motor_forward pin
 #define MOTOR_BACKWARD 11  // pin nummer för motor_backward pin
 
@@ -49,17 +52,9 @@ void set_speed(uint8_t speed)
   digitalWrite(MOTOR_BACKWARD, 0);
 }
 
-void demo()
-{
-  for (int i = 0; i < 200; i++)
-  {
-    motor_forward(i);
-    delay(20);
-  } 
-}
+
 void loop()
 {
-  //demo();
   motor_wait_for_start(); 
   // set_speed(50);
   // delay(100);
